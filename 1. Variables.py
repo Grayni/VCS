ae22ff0683a76b15e8f2a15920ec9bc0a4ebc5fb
Grayni <grayniweb@gmail.com>
2022-02-11 02:11:58 +0300

    Saving file changes ~ 1.Variables.py

--- 1. Variables.py
+++ 1. Variables.py
@@ -1,2 +1,20 @@
-a = 'geek-brains'
-input(a)
+# Variables. Example
+
+string_geek = 'geekbrains'
+num_year = 2022
+print(string_geek, num_year)
+print('----------------')
+print('')
+
+# Request color
+
+color = input('Ваш любимый цвет? ')
+print(f'Узнайте как характеризует Вас "{color} цвет" - https://ocvete.ru/psixologiya-cveta/simvolika-cveta.html')
+print('----------------')
+print('')
+
+# Request number
+number = int(input('Придумайте 4-х значное число "XXXX" \n'))
+print()
+print('Число', number/100, 'в 100 раз меньше числа:', number)
+print('----------------')
